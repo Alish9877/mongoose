c33eaf8a58db32665e5ca564f436103c3e48c606
alish9877 <alshaikhali302@gmail.com>
2024-11-27 09:24:40 +0300

    do

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ const mongoose = require('mongoose');
 const Todo = require('./models/todo')
 const prompt = require('prompt-sync')();
 
-const menu = prompt('Choese the action');
+const menu = console.log('Welcome to the CRM')
 
-console.log(`Your choies is ${menu}`);
+console.log('What would you like to do?')
+
+console.log('1. Create a customer')
+console.log('2. View all customers')
+console.log('3. Update a customer')
+console.log('4. Delete a customer')
+console.log('5. quit')
+
+// console.log('5. quit')
+
+const promenu = prompt('Number of action to run:');
+
+
+// Welcome to the CRM
+
+// What would you like to do?
+
+// 1. Create a customer
+//   2. View all customers
+//   3. Update a customer
+//   4. Delete a customer
+//   5. quit
+
+// Number of action to run: 
+// # user inputs 3
+
+console.log(`Your choies is ${promenu}`);
